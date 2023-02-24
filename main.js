26f3b36e136bdf972b42751c2c7d8a01ce47ea4e
Andrew Hawileh <andrewh829@gmail.com>
2023-02-24 14:56:46 -0600

    done

--- main.js
+++ main.js
@@ -3,9 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
-
-
+const errorModal = document.getElementById('modal')
+const errorMsg = document.getElementById('modal-message')
+errorModal.setAttribute('class', 'hidden');
+const likeButtons = document.querySelectorAll('span.like-glyph');
+likeButtons.forEach((el) => {
+el.addEventListener('click', (e) => {
+  const buttonClicked = e.target;
+    mimicServerCall()
+    .then(() => {
+      /*
+      When a user clicks on a full heart:
+      Change the heart back to an empty heart
+      Remove the .activated-heart class
+      */
+      if (buttonClicked.className == 'activated-heart')
+      {
+          console.log("Un-liking")
+          buttonClicked.setAttribute('class', 'like-glyph')
+          buttonClicked.textContent = '♡';
+      }
+      else
+      {
+        console.log("Liking")
+        buttonClicked.textContent = '♥';
+        buttonClicked.setAttribute('class', 'activated-heart')
+      }
+    })
+    .catch((e) => {
+      errorMsg.textContent = e;
+      errorModal.removeAttribute('class')
+      setTimeout(() => {errorModal.setAttribute('class', 'hidden');errorMsg.textContent = '';}, 3000)
+    })
+})
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
